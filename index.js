cec367c6d032f5f92ec673e76aaa072bf5dd650b
Gerardo Munguia <gerardomunguiahoyo@gmail.com>
2017-10-31 10:16:38 +0100

    Fix bug triggered by missing titles.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = function umlPlugin(md) {
         srcIndex = token.attrIndex('src'),
         titleIndex = token.attrIndex('title'),
         alt = 'ac:alt="' + token.content + '"',
-        title = 'ac:title="' + token.attrs[titleIndex][1] + '"',
+        title = token.attrs[titleIndex] ? 'ac:title="' + token.attrs[titleIndex][1] + '"' : '',
         url = '<ri:url ri:value="' + token.attrs[srcIndex][1] + '" />';
 
     return '<ac:image ' + alt + ' ' + title + '>' + url + '</ac:image>';
